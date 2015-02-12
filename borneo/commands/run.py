7388d88e2c689ca9e588743cb4b543169990e631
Matt Spraggs <matthew.spraggs@gmail.com>
2015-02-12 01:01:51 +0000

    Added usage output to run command

--- borneo/commands/run.py
+++ borneo/commands/run.py
@@ -1,9 +1,15 @@
 from __future__ import absolute_import
+from __future__ import print_function
 
 import importlib
+import sys
 
 
 def main(argv):
     """Run the specified main function in module"""
-    mod = importlib.import_module(argv[0])
+    try:
+        mod = importlib.import_module(argv[0])
+    except IndexError:
+        print("Usage: python {} run <module>".format(sys.argv[0]))
+        sys.exit()
     mod.main()
