40a4632ac60072ce18790af44b2465226639e8dd
Matt Spraggs <matthew.spraggs@gmail.com>
2015-02-12 19:14:01 +0000

    Added pickle_result and unpickle_result functions to data.py

--- borneo/data.py
+++ borneo/data.py
@@ -1,1 +1,23 @@
+from __future__ import absolute_import
 
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+from borneo.paths import make_results_path
+
+
+def pickle_result(filename, data, study):
+    """Pickle the supplied data to the specified file in the given study results
+    directory"""
+    with open(make_results_path(filename, study), 'w') as f:
+        pickle.dump(data, f, protocol=2)
+
+
+def unpickle_result(filename, study):
+    """Load the data from the specified file in the given study results
+    directory"""
+    with open(make_results_path(filename, study)) as f:
+        data = pickle.load(f)
+    return data
